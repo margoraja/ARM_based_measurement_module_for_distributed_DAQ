6f9c5e0edc3a3ad5a83d7c3e7b3dc8e773fa697f
margoraja <margoraja@gmail.com>
2016-05-10 00:51:22 +0300

    Newly build project.

--- Debug/SignalGenerator.pp
+++ Debug/SignalGenerator.pp
@@ -1,5 +1,27 @@
 # FIXED
 
 SignalGenerator.obj: ../SignalGenerator.c
+SignalGenerator.obj: ../ARM_measurement_unit.h
+SignalGenerator.obj: C:/TI-CMSIS-Devices-8636/TI/LM4F/Include/LM4F120H5QR.h
+SignalGenerator.obj: C:/ti/ccsv6/ccs_base/arm/include/CMSIS/core_cm4.h
+SignalGenerator.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdint.h
+SignalGenerator.obj: C:/ti/ccsv6/ccs_base/arm/include/CMSIS/core_cmInstr.h
+SignalGenerator.obj: C:/ti/ccsv6/ccs_base/arm/include/CMSIS/cmsis_ccs.h
+SignalGenerator.obj: C:/ti/ccsv6/ccs_base/arm/include/CMSIS/core_cmFunc.h
+SignalGenerator.obj: C:/ti/ccsv6/ccs_base/arm/include/CMSIS/core_cmSimd.h
+SignalGenerator.obj: C:/TI-CMSIS-Devices-8636/TI/LM4F/Include/system_LM4F.h
+SignalGenerator.obj: ../Timers.h
+SignalGenerator.obj: ../Utils.h
 
 ../SignalGenerator.c: 
+../ARM_measurement_unit.h: 
+C:/TI-CMSIS-Devices-8636/TI/LM4F/Include/LM4F120H5QR.h: 
+C:/ti/ccsv6/ccs_base/arm/include/CMSIS/core_cm4.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdint.h: 
+C:/ti/ccsv6/ccs_base/arm/include/CMSIS/core_cmInstr.h: 
+C:/ti/ccsv6/ccs_base/arm/include/CMSIS/cmsis_ccs.h: 
+C:/ti/ccsv6/ccs_base/arm/include/CMSIS/core_cmFunc.h: 
+C:/ti/ccsv6/ccs_base/arm/include/CMSIS/core_cmSimd.h: 
+C:/TI-CMSIS-Devices-8636/TI/LM4F/Include/system_LM4F.h: 
+../Timers.h: 
+../Utils.h: 
